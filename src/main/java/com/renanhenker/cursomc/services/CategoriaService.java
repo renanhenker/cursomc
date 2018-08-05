31dab6118d52a79e5a221d1428061615c1fd40ec
Renan Henker <renan.henker@gmail.com>
2018-08-05 15:56:23 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/renanhenker/cursomc/services/CategoriaService.java
+++ src/main/java/com/renanhenker/cursomc/services/CategoriaService.java
@@ -7,15 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.renanhenker.cursomc.domain.Categoria;
 import com.renanhenker.cursomc.repositories.CategoriaRepository;
+import com.renanhenker.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria buscar(Integer id) {
+
+
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
