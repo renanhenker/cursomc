b828cf140a69838226c4580fb34a9dccf786d9a1
Renan Henker <renan.henker@gmail.com>
2018-08-05 18:35:34 -0300

    Estado e Cidade

---
+++ src/main/java/com/renanhenker/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.renanhenker.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.renanhenker.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
